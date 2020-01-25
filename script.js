940dd5279e1c38dda46c16518a8bdd52a6252c16
chethansai <40889841+chethansai@users.noreply.github.com>
2020-01-25 18:37:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
         {
             name: 'Magnemite',
             location: {
-                lat: 44.496470,
-                lng: 11.320180,
+                lat: 17.361903,
+                lng: 78.524442,
             }
         },
     ];
